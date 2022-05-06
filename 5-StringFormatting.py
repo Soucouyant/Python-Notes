15fa9c14d35fe0c46f89679de646ddc48f71d530
Soucouyant <duranr@protonmail.com>
2022-05-06 14:40:35 -0400

    finished Part 5

--- 5-StringFormatting.py
+++ 5-StringFormatting.py
@@ -3,3 +3,17 @@
 # Friday May 6 2022 
 # TEJ4M1 P2
 
+# String Formatting
+# In order to use variables within a string the % is used
+# There are different specifiers 
+# %s    | String 
+# %d    | Integers
+# %f    | Float values
+# %.f   | Float values (fixed digit)
+# %x/%X | Integers in hex representation
+# In order to call that set of data use %(dataset)
+data = ("Duran", "Ramlall", 25.6)
+formattedString = "Hello %s %s. Your phone currently has %.1f%% battery left"
+print(formattedString %(data))
+
+# Note, in order to use the % sign to represent the percent I used %%
