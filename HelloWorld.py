652cc75e028be6251860f92a9050ffb9f821dddd
Soucouyant <duranr@protonmail.com>
2022-05-06 10:45:27 -0400

    Initialize

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+# Duran Ramlall 
+# Learn Python - Hello, World
+# Friday May 6 2022
+# TEJ4M1 P2
+print("Hello, World!")
---
+++ VariablesNTypes.py
@@ -0,0 +1,35 @@
+# Duran Ramlall 
+# Learn Python - Variables and Types 
+# Friday May 6 2022 
+# TEJ4M1 P2
+
+# Numbers 
+# Two types of numbers in python, integers and floats
+
+## Integers
+int = 2
+print(int);
+
+## Floats
+## Notation Type 1: Standard Declaration
+float = 2.2
+print(float)
+## Notation Type 2: float method [float is not callable on MAC]
+# float2 = float(2)
+# print(float2)
+
+# Strings
+## Single Quote vs Double Quote
+## Double Quotes "" allow for apostrophes to be easier to implement
+myString = "I can't believe..."
+print(myString)
+
+## String concatenation
+String1 = "Hello,"
+String2 = "World"
+print(String1 + " " + String2)
+
+# Simultaneous Declarations
+x, y = 1, 2
+print(x,y)
+
